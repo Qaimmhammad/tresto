c0ab6ce804f8e5de4d43fbc321c055031eff6e78
Qaim <qaymmhmd58@gmail.com>
2026-09-13 01:29:12 +0300

    test

--- tresto-frontend/src/app/restaurant-registration/appearance/action.ts
+++ tresto-frontend/src/app/restaurant-registration/appearance/action.ts
@@ -5,5 +5,10 @@ import {uploadLogoHelper} from "@/helpers/upload-image";
 export default async function uploadLogo(
   image: File
 ) {
+  console.log("UPLOAD LOGO ACTION:", {
+        name: image.name,
+        size: image.size,
+        type: image.type,
+    });
   return await uploadLogoHelper(image);
 }
